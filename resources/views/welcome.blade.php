749863e7a65028b4ac43d03296459b6272547da8
Ted Carrasco <ted_cc@hotmail.com>
2018-03-20 17:06:16 -0400

    First Message

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="{{ asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse" style="border-radius:0;">
+      <div class="container-fluid">
+        <div class="col-md-1">
+
+        </div>
+        <div class="col-md-10">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div>
+        <div class="col-md-1">
+
+        </div>
+
+      </div><!-- /.container-fluid -->
+    </nav>
+    <div class="container-fluid">
+      <div class="col-md-1">
+
+      </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="col-md-1">
+
+      </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+
+
+    <div class="container-fluid">
+      <div class="col-md-1">
+
+      </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="col-md-1">
+
+      </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="col-md-1">
+
+      </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-2">
+          <iframe width="100%" height="200" src="https://www.youtube.com/embed/3eApJGC02dA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{ asset('bootstrap/js/bootstrap.min.js')}}"></script>
+  </body>
+</html>
